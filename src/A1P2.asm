6b5d3afedda276ff233231914202457f990a2421
gomzalo <2205280200101@ingnieria.usac.edu.gt>
2018-09-30 16:54:03 -0600

    Renaming >:v

---
+++ src/A1P2.asm
@@ -0,0 +1,47 @@
+;mmm
+.model small
+
+.data
+encabezado1         db      "Universidad de San Carlos de Guatemala             ",13,10,"$"
+encabezado2         db      "Facultad de Ingenieria                             ",13,10,"$"
+encabezado3         db      "Escuela de ciencias y sistemas                     ",13,10,"$"
+encabezado4         db      "Arquitectura de Computadores y Ensambladores 1     ",13,10,"$"
+encabezado5         db      "Seccion B                                          ",13,10,"$"
+encabezado6         db      "Segundo semestre 2018                              ",13,10,"$"
+encabezado7         db      "Gonzalo Antonio Garcia Solares                     ",13,10,"$"
+encabezado8         db      "201318652                                          ",13,10,"$"
+encabezado9         db      "Segunda practica                                   ",13,10,"$"
+encabezado10    	db		"___________________________________________________",13,10,"$"
+menu1               db      "1. Cargar archivo                                  ",13,10,"$"
+menu2               db      "2. Crear reporte                                   ",13,10,"$"
+menu3               db      "3. Salir                                           ",13,10,"$"
+
+.code 
+
+mostrarMenu:
+
+    mov ax,@data
+
+    mov ds,ax
+
+    mov ah,09
+    mov dx,offset encabezado1
+    mov dx,offset encabezado2
+    mov dx,offset encabezado3
+    mov dx,offset encabezado4
+    mov dx,offset encabezado5
+    mov dx,offset encabezado6
+    mov dx,offset encabezado7
+    mov dx,offset encabezado8
+    mov dx,offset encabezado9
+    mov dx,offset encabezado10
+    mov dx,offset menu1
+    mov dx,offset menu2
+    mov dx,offset menu3
+    int 21h
+
+    mov ax,4c00h
+    int 21h
+
+end mostrarMenu
+code ends
--- src/A1Practica2.asm
+++
@@ -1,8 +0,0 @@
-;mmm
-.model small
-
-.data
-encabezado1         db      "Universidad de San Carlos de Guatemala             ",13,10,"$"
-encabezado2         db      "Facultad de Ingenieria                             ",13,10,"$"
-encabezado3         db      "Arquitectura de Computadores y Ensambladores 1     ",13,10,"$"
-encabezado4         db      "Seccion B                                          ",13,10,"$"
